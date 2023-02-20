70412b2c86d4806f6d939942229e50fc3e181ba2
albim72 <marcin.albiniak72@gmail.com>
2023-02-20 10:25:15 +0100

    Update funkcje.py

--- DZIEN_1/funkcje.py
+++ DZIEN_1/funkcje.py
@@ -13,3 +13,18 @@ print(multi(6)(3))
 
 #przukład -> filtrowanie listy
 num = [67,2,5,-56,4,0,16,101,-33,56,999,-23,0,4,12,2]
+
+#użyjemy funkcji wyższego rzędu filter(minimum jeden argument to  funkcja)
+parzyste = list(filter(lambda x:x%2==0,num))
+print(parzyste)
+
+#użyj funkcji map do utworzenia nowej listy cube mapując elementy listy num podniesione do pot 3
+
+cube = list(map(lambda x:x**3,num))
+print(cube)
+
+def addfive(x):
+    return x+5
+
+five = list(map(addfive,num))
+print(five)
