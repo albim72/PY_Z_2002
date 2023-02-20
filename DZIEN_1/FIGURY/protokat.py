1b11cc5862deea74dfc6717471129cf70698ce32
albim72 <marcin.albiniak72@gmail.com>
2023-02-20 15:49:24 +0100

    Create protokat.py

---
+++ DZIEN_1/FIGURY/protokat.py
@@ -0,0 +1,14 @@
+from figura import Figura
+from Kwadrat import Kwadrat
+
+class Prostokat(Figura):
+    def __new__(cls,a,b):
+        if a == b:
+            return Kwadrat(bok=a)
+        return object.__new__(Prostokat)
+
+    def policz_pole(self):
+        return self.a*self.b
+
+    def opis(self):
+        pass
