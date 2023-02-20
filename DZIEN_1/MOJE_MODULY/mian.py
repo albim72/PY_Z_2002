7aee28717e7fd34b0375f35fb095356add741785
albim72 <marcin.albiniak72@gmail.com>
2023-02-20 13:41:18 +0100

    Update mian.py

--- DZIEN_1/MOJE_MODULY/mian.py
+++ DZIEN_1/MOJE_MODULY/mian.py
@@ -3,6 +3,7 @@
 from dane import nbf as filia
 from dane import book as ksiazka
 from funkcje.bfunkcje import czytajliste,czytajslownik
+from klasy.KDane import DKlasa
 
 print("_______wypisanie bezpośrednio_________")
 print(filia)
@@ -11,3 +12,8 @@ print(ksiazka)
 print("_______wypisanie za pomocą funckji_________")
 czytajliste(filia)
 czytajslownik(ksiazka)
+
+print("_______wypisanie za pomocą klasy_________")
+dk = DKlasa(filia,ksiazka)
+dk.czytajs()
+dk.czytajl()
