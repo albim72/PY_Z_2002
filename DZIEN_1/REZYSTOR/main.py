7bcb4029ee0a6319c5d53b8caab2a1473caee8bb
albim72 <marcin.albiniak72@gmail.com>
2023-02-20 14:05:20 +0100

    Update main.py

--- DZIEN_1/REZYSTOR/main.py
+++ DZIEN_1/REZYSTOR/main.py
@@ -1,6 +1,25 @@
 from oldresistor import OldResistor
+from resistor import Resistor
+from voltage import VoltageResistance
+
 
 r0 = OldResistor(50e3)
+print(f"____________{r0.__class__.__name__}______________")
 print(f'przed zmianą: {r0.get_ohms()} omów')
 r0.set_ohms(14e2)
 print(f'po zmianie: {r0.get_ohms()} omów')
+
+r1 = Resistor(34e3)
+print(f"____________{r1.__class__.__name__}______________")
+r1.ohms = 10e3
+print(f'oporność: {r1.ohms} omów,\n'
+      f'napięcie prądu: {r1.voltage} V\n'
+      f'natężenie prądu: {r1.current} A\n')
+
+r2 = VoltageResistance(1e2)
+print(f'natężenie początkowe prądu: {r2.current:.2f} A')
+print(f'napięcie początkowe prądu: {r2.voltage:.2f} V')
+r2.voltage  = 70
+
+print(f'natężenie prądu: {r2.current:.2f} A')
+print(f'napięcie prądu: {r2.voltage:.2f} V')
