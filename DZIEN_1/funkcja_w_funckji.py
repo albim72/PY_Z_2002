43e5fb3876306b8406170e4cb29825ebd65b626e
albim72 <marcin.albiniak72@gmail.com>
2023-02-20 11:31:17 +0100

    Update funkcja_w_funckji.py

--- DZIEN_1/funkcja_w_funckji.py
+++ DZIEN_1/funkcja_w_funckji.py
@@ -1,60 +1,40 @@
-#przykład 1
+import math
+import time
 
-def witaj(imie):
-    return f"miło Cię widzieć {imie}"
-
-def konkurs(imie,punkty):
-    return f"uczetnik konkursu {imie} otrzymał {punkty} punktów"
-
-def osoba(funkcja,*args):
-    return funkcja(*args)
-
-print(osoba(witaj,"Leon"))
-print(osoba(konkurs,"Olga",67))
-
-
-#przykład 2
-
-def rejestracja(oplata):
-    def lista():
-        return "jesteś na liście zawodników"
-    def brak():
-        return "jeśli nie uiścisz opłaty startowej zostaniesz usunięty z listy startowej"
-    def error():
-        return "brak potwierdzenia, podaj wartość 1 -> opłata, 0->brak opłaty"
-
-    if oplata == 1:
-        return lista
-    elif oplata == 0:
-        return brak
-    else:
-        return error
-
-print(rejestracja(1)())
-print(rejestracja(0)())
-print(rejestracja(13)())
-
-#przykład 3
-
-def startstop(funkcja):
-    def wrapper(*args):
-        print("rozpoczęcie procesu...")
-        funkcja(*args)
-        print("zakończenie procesu...")
+def pomiarczasu(funkcja):
+    def wrapper():
+        starttime = time.perf_counter()
+        funkcja()
+        endtime = time.perf_counter()
+        print(f"czasy wykonania funkcji {funkcja.__name__} -> {endtime-starttime} s")
     return wrapper
 
-def zawijanie(czego):
-    print(f"zawijanie {czego} w sreberka...")
-
-zw = startstop(zawijanie)
-zw("czekoladek")
-
-@startstop
-def dmuchanie(czego):
-    print(f"dmuchanie {czego} w urodziny...")
+def sleep(funkcja):
+    def wrapper():
+        time.sleep(3)
+        return funkcja()
+    return wrapper
 
 
-dmuchanie("świeczek")
+@pomiarczasu
+@sleep
+def big_lista():
+    sum([i**7 for i in range(1000000)])
+
+big_lista()
+
+def debug(funkcja):
+    def wrapper(*args,**kwargs):
+        print(f'wołana funckja: {funkcja.__name__}')
+        funkcja(*args,**kwargs)
+    return wrapper
+
+@debug
+def info(inn):
+    print(f"informacja: {inn}")
+
+
+info("ABC 123")
 
 
 
