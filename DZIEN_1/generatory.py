87ab717792ae0da0820c79e02b27dd0a0ba99e94
albim72 <marcin.albiniak72@gmail.com>
2023-02-20 12:00:26 +0100

    Update generatory.py

--- DZIEN_1/generatory.py
+++ DZIEN_1/generatory.py
@@ -37,3 +37,26 @@ for i in wznowienie(6,8):
     print(f"zwróconono wartość i = {i}")
 
 
+def complexgen():
+    x=0
+    while True:
+        print("x-print1")
+        y = yield x
+        print("x-print2")
+        if y is None:
+            x = x+1
+        else:
+            x=y
+
+g= complexgen()
+print(next(g))
+print(next(g))
+print(next(g))
+print(next(g))
+print(g.send(115))
+print(next(g))
+print(next(g))
+print(next(g))
+print(next(g))
+
+
