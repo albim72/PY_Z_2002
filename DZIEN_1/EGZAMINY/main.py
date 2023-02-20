14124c54c9789f870b7773c34aa8a00f3a7d9053
albim72 <marcin.albiniak72@gmail.com>
2023-02-20 14:46:32 +0100

    Update main.py

--- DZIEN_1/EGZAMINY/main.py
+++ DZIEN_1/EGZAMINY/main.py
@@ -1,7 +1,16 @@
 from homework import Homework
+from exam import Exam
 
 print("_____________ HOMEWORK _____________")
 uc1 = Homework()
 uc1.grade = 89
 assert uc1.grade == 89
 print(uc1.grade)
+
+print("_____________ EXAM _____________")
+uc2 = Exam()
+uc2.writing_grade = 80
+uc2.math_grade = 62
+
+print(f'writing: {uc2.writing_grade}')
+print(f'math: {uc2.math_grade}')
