1ebe3954d030831d8794871f1af9a0b147e6e76a
albim72 <marcin.albiniak72@gmail.com>
2023-02-21 11:10:21 +0100

    Create main.py

---
+++ DZIEN_2/DICTERROR/main.py
@@ -0,0 +1,11 @@
+from CustomDict import CustomIntFloatDict
+
+test1 = CustomIntFloatDict()
+print(test1)
+
+# test2 = CustomIntFloatDict({'a','b'},[1,6])
+
+# test3 = CustomIntFloatDict(('x','y','z'),(10,'twenty',30))
+
+test4 = CustomIntFloatDict(('x','y','z'),(10,20,30))
+print(test4)
