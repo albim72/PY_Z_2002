e3e46702e7b9b2d064ecf7162a8ac5c1689fd677
albim72 <marcin.albiniak72@gmail.com>
2023-02-23 15:01:59 +0100

    Update af_game.py

--- DZIEN_4/DESIGN_PATTERNS/af_game.py
+++ DZIEN_4/DESIGN_PATTERNS/af_game.py
@@ -68,14 +68,34 @@ class WizardWorld:
 
     def make_obstacle(self):
         return Ork()
-    
-    
+
+
 #Game environment
 
 class GameEnvironment:
     def __init__(self,factory):
         self.hero = factory.make_character()
         self.obstacle = factory.make_obstacle()
-        
+
     def play(self):
         self.hero.interact_with(self.obstacle)
+
+def validate_age(name):
+    try:
+        age = int(input(f"Welcome {name}. How old are You? "))
+    except ValueError as er:
+        print(f"Age {age} is invalid. Please try again...")
+        return (False,age)
+    return (True,age)
+
+def main():
+    name = input("Hello, What's Your name? ")
+    valid_input = False
+    while not valid_input:
+        valid_input, age = validate_age(name)
+    game = FrogWorld if age <12 else WizardWorld
+    environment = GameEnvironment(game(name))
+    environment.play()
+
+if __name__ == '__main__':
+    main()
