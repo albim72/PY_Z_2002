9ee6bf86158e678e2ae293d07e899f9b9635af25
albim72 <marcin.albiniak72@gmail.com>
2023-02-24 09:16:26 +0100

    Create pizza.py

---
+++ DZIEN_5/PIZZERIA/pizza.py
@@ -0,0 +1,17 @@
+import time
+
+class Pizza:
+    def __init__(self,name):
+        self.name = name
+        self.dough = None
+        self.sauce = None
+        self.topping = []
+        
+    def __str__(self):
+        return self.name
+    
+    def prepare_dough(self,dough):
+        self.dough = dough
+        print(f'przygotowanie {self.dough.name} -> ciasto na {self}')
+        time.sleep(STEP_DELAY)
+        print(f'wykonanane na {self.dough.name} ciasto')
