a990f50a499dd62d67ee3f8fa1891e2454ec4817
albim72 <marcin.albiniak72@gmail.com>
2023-02-24 11:26:14 +0100

    Update request_1.py

--- DZIEN_5/WEBSCRAPING/request_1.py
+++ DZIEN_5/WEBSCRAPING/request_1.py
@@ -1,5 +1,9 @@
 import urllib3
 from bs4 import BeautifulSoup
+from lxml import html
 
 http = urllib3.PoolManager()
 r = http.request('GET','https://authoraditiagarwal.com')
+soup = BeautifulSoup(r.data,'lxml')
+print(soup.title)
+print(soup.title.text)
