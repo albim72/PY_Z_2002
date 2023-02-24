1a57e3f2afe0b95d48b09e9e67f8bc4a8f5c318d
albim72 <marcin.albiniak72@gmail.com>
2023-02-24 09:57:05 +0100

    Update main.py

--- DZIEN_5/PIZZERIA/main.py
+++ DZIEN_5/PIZZERIA/main.py
@@ -1,11 +1,34 @@
-from enum import Enum
-import time
+from margaritabulder import MargaritaBuilder
+from creamybaconbuilder import CreamyBaconBuilder
+from waiter import Waiter
 
-PizzaProgress = Enum('PizzaProgress','w kolejce do wypieku....')
-PizzaDough = Enum('PizzaDough','cienkie ciasto')
-PizzaSauce = Enum('PizzaSouce','kremowy sos pomidorowy')
-PizzaTopping = Enum('PizzaTopping','mozarella double_mozarella boczek szynka '
-                                   'grzyby czerwona_cebula oregano')
 
-STEP_DELAY = 3 #w sekundach
 
+
+def validate_style(builders):
+    try:
+        input_msg = 'Jaką Pizzę wyboerasz, [m]argarita czy [c]reamy bacon? '
+        pizza_style = input(input_msg)
+        builder = builders[pizza_style]()
+        valid_input = True
+    except KeyError:
+        error_msg = 'Sorry, tylko [m]argarita czy [c]reamy bacon są dziś dostępne...'
+        print(error_msg)
+        return (False,None)
+    return (True,builder)
+
+
+def main():
+    builders = dict(m=MargaritaBuilder,c=CreamyBaconBuilder)
+    valid_input = False
+    while not valid_input:
+        valid_input,builder = validate_style(builders)
+    print()
+    waiter = Waiter()
+    waiter.construct_pizza(builder)
+    pizza = waiter.pizza
+    print()
+    print(f'Ciesz się swoją pizzą, Smacznego!')
+
+if __name__ == '__main__':
+    main()
